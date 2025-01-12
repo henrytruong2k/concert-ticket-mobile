7472fa46901c5f048c9ef825eb8d51e3e142e81e
huytruong <kzyo5962@gmail.com>
2025-01-12 15:54:17 +0700

    init

---
+++ app/(authed)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Redirect, Stack } from "expo-router";
+
+export default function AppLayout() {
+  //check from context if user is logged in
+  const isLoggedIn = true;
+
+  if (!isLoggedIn) {
+    return <Redirect href="/login" />;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
