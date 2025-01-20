b332cc3bc18ad58d33bb1f556f79c2c3fc227bda
huytruong <kzyo5962@gmail.com>
2025-01-20 23:50:08 +0700

    custom library UI

--- app/(authed)/(tabs)/(scan-ticket)/_layout.tsx
+++ app/(authed)/(tabs)/(scan-ticket)/_layout.tsx
@@ -1,5 +1,46 @@
+import { Button } from "@/components/Button";
+import { VStack } from "@/components/VStack";
+import {
+  BarcodeScanningResult,
+  CameraView,
+  useCameraPermissions,
+} from "expo-camera";
 import React from "react";
+import { ActivityIndicator, Text } from "react-native";
 
 export default function ScanTicketScreen() {
-  return <></>;
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    return (
+      //loading state
+      <VStack flex={1} justifyContent="center" alignItems="center">
+        <ActivityIndicator size={"large"} />
+      </VStack>
+    );
+  }
+
+  if (!permission.granted) {
+    return (
+      <VStack gap={20} flex={1} justifyContent="center" alignItems="center">
+        <Text>Camera access is required to scan tickets.</Text>
+        <Button onPress={requestPermission}>Allow Camera Access</Button>
+      </VStack>
+    );
+  }
+
+  function onBarcodeScanned({ data }: BarcodeScanningResult) {
+    console.log(data);
+  }
+
+  return (
+    <CameraView
+      style={{ flex: 1 }}
+      facing="back"
+      onBarcodeScanned={onBarcodeScanned}
+      barcodeScannerSettings={{
+        barcodeTypes: ["qr"],
+      }}
+    />
+  );
 }
